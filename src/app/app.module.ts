90b7e1d688247d7e73ceb9d51afc22ea71284394
rodrigo-contreras-sf <rodry2580@gmail.com>
2022-10-14 17:28:21 -0300

    navbar + logo + ruta por defecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
