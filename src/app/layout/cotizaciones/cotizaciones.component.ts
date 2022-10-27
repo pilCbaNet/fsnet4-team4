b04c7a3fe386f38bb769dea0d565fab7d3c62608
Agustin Del Giudice <dgagustin@outlook.com>
2022-10-27 00:05:05 -0300

    hubo cambios de nombres en la tabla cotizaciones

--- src/app/layout/cotizaciones/cotizaciones.component.ts
+++ src/app/layout/cotizaciones/cotizaciones.component.ts
@@ -27,7 +27,7 @@ export class CotizacionesComponent implements OnInit {
 
   leerMas() {
     const element: HTMLElement = document.getElementById('hideTextBtn') as HTMLElement
-    element.innerHTML = 'El volumen de comercio de criptomonedas total en el último día es de <span class="text-success">100 mil millones $.</span> El dominio de Bitcoin es del <span class="text-success">40 %</span>, mientras que el dominio de <span>Ethereum</span> es del <span class="text-success">20 %</span>. En este momento, <span>CoinGecko</span> rastrea <span class="text-success">13.260 criptomonedas</span>. Las tendencias populares del sector son <span class="text-success">Finance / Banking </span> y <span class="text-success">Decentralized Exchange (DEX).</span>'
+    element.innerHTML = '<p>El volumen de comercio de criptomonedas total en el último día es de <span class="text-success">100 mil millones $.</span> El dominio de Bitcoin es del <span class="text-success">40 %</span>, mientras que el dominio de <span>Ethereum</span> es del <span class="text-success">20 %</span>. En este momento, <span>CoinGecko</span> rastrea <span class="text-success">13.260 criptomonedas</span>. Las tendencias populares del sector son <span class="text-success">Finance / Banking </span> y <span class="text-success">Decentralized Exchange (DEX).</span></p>'
     element.style.textDecoration = 'none';
     element.style.color = 'grey';
     element.style.cursor = 'text';
@@ -37,10 +37,10 @@ export class CotizacionesComponent implements OnInit {
   filteredCoins: Coin[] = []
   titles: string [] = [
     '#',
-    'Coin',
-    'Price',
-    'Price Change',
-    '24 h Volume'
+    'Moneda',
+    'Precio',
+    'Cambio de Precio',
+    'Volumen en 24 h'
   ]
 
   searchText = '';
