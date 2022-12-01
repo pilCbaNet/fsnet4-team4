0361298dcb61b0ad8aa3707e5844558c10aa4aa6
Marcos Marin <114090735+MarcosMarin25@users.noreply.github.com>
2022-12-01 18:57:53 -0300

    se agrego monedacontroller
    se agrego  controllador moneda con get y post

---
+++ BackEnd/CryptoPIL/CryptoPIL/Controllers/MonedasController.cs
@@ -0,0 +1,30 @@
+﻿using CryptoPIL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoPIL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonedasController : ControllerBase
+    {
+        [HttpGet]
+        public List<Moneda> Get()
+        {
+            using(var db = new CryptoPILContext())
+            {
+                return db.Monedas.ToList();
+            }
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Moneda oMoneda)
+        {
+            using (var db = new CryptoPILContext())
+            {
+                db.Monedas.Add(oMoneda);
+                db.SaveChanges();
+            }
+        }
+    }
+}
