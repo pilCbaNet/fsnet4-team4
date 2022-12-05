8ff3d3470404ed5a1344b71d35ac5ab587f7ff5c
Marcos Marin <114090735+MarcosMarin25@users.noreply.github.com>
2022-12-05 10:29:16 -0300

    se agrega metodo get por id y put en mondeascontroller

--- BackEnd/CryptoPIL/CryptoPIL/Controllers/MonedasController.cs
+++ BackEnd/CryptoPIL/CryptoPIL/Controllers/MonedasController.cs
@@ -1,6 +1,7 @@
 ﻿using CryptoPIL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CryptoPIL.Controllers
 {
@@ -11,11 +12,42 @@ namespace CryptoPIL.Controllers
         [HttpGet]
         public List<Moneda> Get()
         {
-            using(var db = new CryptoPILContext())
+            using (var db = new CryptoPILContext())
             {
                 return db.Monedas.ToList();
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetTarjeta(int id)
+        {
+            try
+            {
+                using (var db = new CryptoPILContext())
+                {
+                    var listMonedas = await db.Monedas.ToListAsync();
+                    var este = new Moneda();
+                    foreach (var item in listMonedas)
+                    {
+                        if (item.IdMoneda == id)
+                        {
+                            este = item;
+                        }
+                    }
+                    return Ok(este);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
 
         [HttpPost]
         public void Post([FromBody] Moneda oMoneda)
@@ -23,6 +55,18 @@ namespace CryptoPIL.Controllers
             using (var db = new CryptoPILContext())
             {
                 db.Monedas.Add(oMoneda);
+                db.SaveChanges();
+            }
+        }
+
+
+        [HttpPut]
+        public void Put([FromBody] Moneda oMoneda)
+        {
+            using (var db = new CryptoPILContext())
+            {
+                Moneda? monedaVieja = db.Monedas.FirstOrDefault(a=> a.IdMoneda == oMoneda.IdMoneda);
+                monedaVieja.Nombre = oMoneda.Nombre;
                 db.SaveChanges();
             }
         }
--- BackEnd/CryptoPIL/CryptoPIL/Program.cs
+++ BackEnd/CryptoPIL/CryptoPIL/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
