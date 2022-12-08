610d95fbc3b65d575e3b6b16851bdabe8ea28589
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2022-12-08 11:54:12 -0300

    agrego metodo Delete del endpoint MonedasDeCuenta

--- BackEnd/CryptoPIL/CryptoPIL/Controllers/MonedasDeCuentaController.cs
+++ BackEnd/CryptoPIL/CryptoPIL/Controllers/MonedasDeCuentaController.cs
@@ -35,20 +35,70 @@ namespace CryptoPILWebApi.Controllers
 
         // POST api/<MonedasDeCuentaController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post(MonedasDeCuenta monedasDeCuenta)
         {
+            using (var db = new CryptoPILContext())
+            {
+                var existeMonedaDeCuenta = db.MonedasDeCuenta.FirstOrDefault(x => x.IdMonedasDeCuenta == monedasDeCuenta.IdMonedasDeCuenta);
+                if (existeMonedaDeCuenta != null)
+                {
+                    return BadRequest("La moneda de cuenta ya existe.");
+                }
+
+                db.Add(monedasDeCuenta);
+                db.SaveChanges();
+                return Ok("Moneda de cuenta creada con exíto");
+            }
         }
 
+
         // PUT api/<MonedasDeCuentaController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{IdMonedasDeCuenta}")]
+        public string Put(int IdMonedasDeCuenta, [FromBody] MonedasDeCuenta monedaDeCuentaActualizada)
         {
+
+            try
+            {
+                using (var db = new CryptoPILContext())
+                {
+                    var monedaDeCuentaModificar = db.MonedasDeCuenta.FirstOrDefault(x => x.IdMonedasDeCuenta == IdMonedasDeCuenta);
+                    if (monedaDeCuentaModificar != null)
+                    {
+                        monedaDeCuentaModificar.Unidades = monedaDeCuentaActualizada.Unidades;
+                        monedaDeCuentaModificar.SaldoMoneda = monedaDeCuentaActualizada.SaldoMoneda;
+
+                        db.SaveChanges();
+                        return "la moneda de cuenta fue  modificada con éxito";
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return "Error al modificar la moneda de cuenta";
         }
 
         // DELETE api/<MonedasDeCuentaController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{IdMonedasDeCuenta}")]
+        public ActionResult Delete(int IdMonedasDeCuenta)
         {
+            using (var db = new CryptoPILContext())
+            {
+                var existeMonedaDeCuenta = db.MonedasDeCuenta.FirstOrDefault(x => x.IdMonedasDeCuenta == IdMonedasDeCuenta);
+                if (existeMonedaDeCuenta == null)
+                {
+                    return NotFound();
+                }
+
+                db.Remove(new MonedasDeCuenta() { IdMonedasDeCuenta = IdMonedasDeCuenta });
+                db.SaveChanges();
+                return Ok();
+
+            }
+
         }
     }
 }
